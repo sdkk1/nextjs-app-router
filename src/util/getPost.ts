8acd2f59f182552a4eb68d66e047fbc9746c4223
Keita Nishimura <fast.break.baske03@gmail.com>
2023-07-02 22:43:14 +0900

    update: getPostのoptionsを追加

--- src/util/getPost.ts
+++ src/util/getPost.ts
@@ -6,17 +6,26 @@ type Post = {
   body: string
   userId: number
 }
+type Options = {
+  noCache?: boolean
+  revalidate?: number
+}
 
-export const getPost = async (): Promise<Post> => {
+export const getPost = async (options?: Options): Promise<Post> => {
   // NOTE: 意図的に3s処理を遅延させる
   await new Promise((resolve) => setTimeout(resolve, 3000))
 
   const randomNumber = getRandomNumber()
+  const cacheStrategy = options?.noCache ? 'no-store' : 'force-cache'
+  const revalidateTime = options?.revalidate ? options.revalidate : undefined
 
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${randomNumber}`,
     {
-      cache: 'no-store',
+      cache: cacheStrategy,
+      next: {
+        revalidate: revalidateTime,
+      },
     }
   )
   const data = res.json()
