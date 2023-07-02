b00243efe3605afe2ef692333fe7729ae1b61df3
Keita Nishimura <fast.break.baske03@gmail.com>
2023-07-02 22:45:54 +0900

    update: 各リンクを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,30 @@ const Home = () => {
   return (
     <main className='grid gap-y-5'>
       <h1>Hello Next.js!</h1>
-      <Link href='/rsc'>to RSC</Link>
-      <Link href='/rcc'>to RCC</Link>
+      <Link
+        href='/rcc'
+        prefetch={false}
+      >
+        to RCC
+      </Link>
+      <Link
+        href='/rsc-static'
+        prefetch={false}
+      >
+        to RSC(Static Data Fetching)
+      </Link>
+      <Link
+        href='/rsc-revalidate'
+        prefetch={false}
+      >
+        to RSC(Revalidate Data Fetching)
+      </Link>
+      <Link
+        href='/rsc-dynamic'
+        prefetch={false}
+      >
+        to RSC(Dynamic Data Fetching)
+      </Link>
       <span className='w-14'>
         <RouterRefreshButton />
       </span>
