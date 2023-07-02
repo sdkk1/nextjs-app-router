fefe61627f0d4111e284ebcfb8a7f14808ca6742
Keita Nishimura <fast.break.baske03@gmail.com>
2023-07-02 22:44:57 +0900

    add: RscStaticを追加

---
+++ src/app/rsc-static/_component/ServerComponent.tsx
@@ -0,0 +1,8 @@
+import { getPost } from '@/util/getPost'
+
+const ServerComponent = async () => {
+  const post = await getPost({ noCache: false })
+  return <p>{post.title}</p>
+}
+
+export default ServerComponent
